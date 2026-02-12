4d408f7ca6d12f3ed0913a47c35d542e51525e4c
m4dd0c <manishsuthar078@gmail.com>
2026-02-12 19:13:00 +0530

    feat: toast and useMediaQuery added

---
+++ client/hooks/useMediaQuery.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+
+// Custom hook for media query -- shadcn
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = React.useState(false);
+
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    setMatches(mediaQuery.matches);
+
+    const handler = (event: MediaQueryListEvent) => setMatches(event.matches);
+    mediaQuery.addEventListener("change", handler);
+    return () => mediaQuery.removeEventListener("change", handler);
+  }, [query]);
+
+  return matches;
+};
+export default useMediaQuery;
