81bdf0e30737e284818620dd53fb5a1be431bbaa
m4dd0c <manishsuthar078@gmail.com>
2026-02-12 19:16:50 +0530

    feat: wholesale page added

---
+++ client/app/(screen)/wholesale/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="grid h-screen w-full place-items-center bg-black text-white">
+      COMING SOON...
+    </div>
+  );
+};
+
+export default Page;
